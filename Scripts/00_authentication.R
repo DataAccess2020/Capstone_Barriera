7e60cd21cbd0c1483227790022b9315e4f8203b3
gaiabarriera <gaia.barriera@studenti.unimi.it>
2022-12-27 02:02:45 +0100

    Hypothesis

--- Scripts/00_authentication.R
+++ Scripts/00_authentication.R
@@ -8,9 +8,11 @@ library(httr)
 library(stringr)
 library(cowplot)
 library(dplyr)
+library(tidyr)
 
-Sys.setenv(SPOTIFY_CLIENT_ID = '1acdf91f2d3a4fe089539ff0d3f0392b')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'b49411cdd0834f6d85c9374c2062f371')
+
+client_credentials <- Sys.setenv(SPOTIFY_CLIENT_ID = '1acdf91f2d3a4fe089539ff0d3f0392b') + 
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = 'b49411cdd0834f6d85c9374c2062f371')
 
 access_token <- get_spotify_access_token()
 
--- Scripts/01_playlists.R
+++ Scripts/01_playlists.R
@@ -1,43 +1,17 @@
 
-#HYPOTHESIS
-# TIKTOK has become a huge phenomenon in the last 2/3 years,
-# thanks to the app some artists trended at world wide level with
-# their songs. The idea behind the project is to take two main playlists 
-# - Top Hits and Tiktok trending - and compare the hit, trying to see how
-# many tiktok's trending hits made it to the top hits.
-# The starting hypothesis is that tiktok became a trendsetter in 
-# matter of music and neW/old artists.
+#HYPOTHESIS ------------------
+# The level of explicit content in music has increased over time and 
+# especially with new artists. 
+# The idea behind this project is to take 4 pillars in the pop-music scene, 
+# starting from the 60-70's till our days. The artists that were selected
+# are: The Beatles (60-70's), Sting (70-90's), Britney Spears (90's-2010) 
+# and Miley Cyrus (2010-2020).
 
-#exporting playlists
+# Getting audio features ----------------
 
-# tiktok trending -----------------
+THE_BEATLES <- get_artist_audio_features("The Beatles")
+STING <- get_artist_audio_features("STING")
+BRITNEY_S <- get_artist_audio_features("Britney Spears")
+MILEY_C <- get_artist_audio_features("Miley Cyrus")
 
-TIKTOK_trending_2022 <- 'https://open.spotify.com/playlist/4wT3RovtJTBRjCS5pIrPU7?si=80fada2d765545da' %>%
-  str_remove(., 'https://open.spotify.com/playlist/')
-
-url_tiktoktrend22 <- str_c('https://api.spotify.com/v1/playlists/', TIKTOK_trending_2022, '/tracks')
-
-tracks_tiktok<- GET(url = url_tiktoktrend22,
-                 config = add_headers(authorization = token))
-tracks_tiktok <- httr::content(tracks_tiktok)
-tracks_id_tiktok <- vector(length = 167) %>% 
-  for (i in 1:167)  {
-    tracks_id_tiktok[[i]] = tracks_tiktok$items[[i]]$track$id
-  }
-
-# top hits -------
-
-
-TOPHITS_2022 <- 'https://open.spotify.com/playlist/7kXId9O5f13vCcLVISaoSk?si=ad38f138479d4384' %>%
-  str_remove(., 'https://open.spotify.com/playlist/')
-
-url_tophits22 <- str_c('https://api.spotify.com/v1/playlists/', TOPHITS_2022, '/tracks')
-
-tracks_tophits<- GET(url = url_tophits22,
-                    config = add_headers(authorization = token))
-tracks_tophits <- httr::content(tracks_tophits)
-tracks_id_tophits <- vector(length = 45) %>% 
-  for (i in 1:45)  {
-  tracks_id_tophits[[i]] = tracks_tophits$items[[i]]$track$id
-}
 
